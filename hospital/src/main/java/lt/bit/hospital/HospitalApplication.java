d87abf6fcc1dfb7d9e91d0665d80cfcedabe3a7b
Gediminas <misev.gediminas@gmail.com>
2018-11-07 16:58:14 +0200

    Pdf microservice

--- hospital/src/main/java/lt/bit/hospital/HospitalApplication.java
+++ hospital/src/main/java/lt/bit/hospital/HospitalApplication.java
@@ -1,18 +1,16 @@
 package lt.bit.hospital;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import lt.bit.hospital.recipe.RecipePdfWriter;
 
 @SpringBootApplication
 public class HospitalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HospitalApplication.class, args);
-		
-		System.out.println("PASILEIDZIA beveik, bet nesipullina");
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HospitalApplication.class, args);
+
+    System.out.println("PASILEIDZIA beveik");
+
+
+  }
 }
---
+++ hospital/src/test/java/lt/bit/hospital/recipe/RecipePdfWriterTest.java
@@ -0,0 +1,46 @@
+package lt.bit.hospital.recipe;
+
+import static org.mockito.Mockito.spy;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
+import lt.bit.hospital.patient.PatientAddress;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RecipePdfWriterTest {
+
+	private Recipe recipe;
+	RecipePdfWriter writer = spy(new RecipePdfWriter(new RestTemplate()));
+//	@Mock
+//	private RecipePdfWriter writer;
+	
+	@Before
+	public void setUp() {
+		recipe = new Recipe();
+		recipe.setDrugDose(55f);
+		recipe.setDrugName("apelsinas");
+		PatientAddress address = new PatientAddress();
+		address.setFlatNumber(1).setStreetName("Zirmunu").setBuildingNumber("5B");
+		recipe.setPatientAddress(address);
+		recipe.setPatientBirthday(LocalDateTime.now());
+		recipe.setPatientId(UUID.randomUUID());
+		recipe.setPatientLastName("pavardenis pavardenis pavardenis");
+		recipe.setPatientName("vardenis");
+		recipe.setRecipeId(UUID.randomUUID());
+	}
+	
+	@Test
+	public void givenRecipeExists_whenWritingPdf_thenPdfIsCreated() throws IOException {
+		
+			writer.writePdf(recipe);
+			
+		
+	}
+
+}
